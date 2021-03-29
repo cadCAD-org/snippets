e7baa18f229d1bac9a106f7b9d9b2047961268d5
Emanuel Lima <emanuel-lima@outlook.com>
2021-03-29 16:30:02 -0300

    Add Menu Creation Workflow

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -6,7 +6,8 @@ export PATH="/github/home/.local/bin:$PATH"
 
 apt-get update && apt-get install -y pandoc \
     texlive-xetex texlive-fonts-recommended \
-    texlive-generic-recommended
+    texlive-generic-recommended build-essential \
+    python-dev python3-dev
 
 pip3 install --user --no-cache-dir -r requirements.txt
 
@@ -15,5 +16,5 @@ cd snippets
 mkdir -p output/pdf
 mkdir -p output/html
 
-jupyter nbconvert --to pdf --execute *.ipynb --output-dir output/pdf
-jupyter nbconvert --to html --execute *.ipynb --output-dir output/html
+jupyter nbconvert --to pdf  *.ipynb --output-dir output/pdf
+jupyter nbconvert --to html  *.ipynb --output-dir output/html
